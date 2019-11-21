f5b38be6df695cd3771e7622f22e1fe4f51c2d0d
Hee Hwang <heehcs@gmail.com>
2019-11-21 11:58:51 -0500

    Replicate Dinora's distribution

--- main.py
+++ main.py
@@ -4,16 +4,46 @@ from sys import stdin, stdout
 from collections import Counter
 from collections import defaultdict
 
-#path = '/home/heeh/Dropbox/F19_LING696/Project/ton/'
-path = os.getcwd() + "/ton/"
-
-
+path = os.getcwd() + "/dainora/"
 stoptokens = ['HiF0', '*', '<', '>', '%r', '24.67']
-def main() -> None:
+
+
+nfinal_ip_tokens = Counter()
+final_ip_tokens = Counter()
+
+nf_ip_phrase = []
+final_ip_phrase = []
+ip_phrase = []
+
+end_symbols = set(['H-H%','H-L%','L-H%','L-L%','!H-L%'])
+ip_end_symbols = set(['H-','L-','!H-'])
+collapse_cand = set(['!H*','L+!H*','L*+!H','!H-', '!H-L%'])
+
+token_counts = Counter()
+uni_counter = Counter()
+ip_counter = defaultdict(Counter)
+ending_counter = defaultdict(Counter)
+uni_lm = defaultdict(Counter)
+ip_lm = defaultdict(Counter)
+ending_lm = defaultdict(Counter)
+
+
+def printDist(distname, counter, lm):
+    print('---------------------------')
+    print(distname)
+    print('---------------------------')
+    print('%6s | %6s | %s | %s' % ('Prev','Cur','freq', 'Prob'))
+    print('---------------------------')
+    for prev,d in lm.items():
+        for cur,v in sorted(d.items(),key=lambda t: -t[1]):
+            print('%6s | %6s | %4d |%2.2f' % (prev, cur, counter[prev][cur], lm[prev][cur]*100))
+        print('---------------------------')                
+
+
+def analyze() -> None:
     tokens = []
 
     for filename in os.listdir(path):
-        # print(filename)
         inf = open(path + filename, "r")
         lines = inf.readlines()[8:]
         for i in range(len(lines)):
@@ -22,113 +52,90 @@ def main() -> None:
                 if('?' not in target[2] and target[2] not in stoptokens):
                     tokens.append(target[2])
 
-    # for i in range(len(tokens)):
-    #     print(tokens[i])
-    vocab = list(set(tokens))
-    
-    phrases = []
-    ips = []
-    end_symbols = set(['H-H%','H-L%','L-H%','L-L%'])
-    ip_end_symbols = set(['H-','L-'])
     ph = ''
+    big_ph = ''
     for token in tokens:
+        # Collapse downstep symbols by Dinora
+        if token in collapse_cand:
+            token = token.replace('!','')
         ph += ' ' + token
+        big_ph += ' ' + token
         if token in end_symbols:
-            phrases.append(ph)
+            # Intonational phrase that includes multiple non-final IPs and one final IP.
+            ip_phrase.append(big_ph)
+            big_ph = ''
+
+            final_ip_phrase.append(ph)
+            final_ip_tokens.update(ph.split())
             ph = ''
+
         elif token in ip_end_symbols:
-            ips.append(ph)
+            nf_ip_phrase.append(ph)
+            nfinal_ip_tokens.update(ph.split())
             ph = ''
-        
-    # for i, ph in enumerate(phrases):
-    #     print(str(i) + ph)
-
-    for s in phrases:
-        s.split(' ')
-
-    for s in ips:
 
 
 
+    print('# of ip phrases:')
+    print(len(ip_phrase))
+
+    print('# of non-final ip phrases:')
+    print(len(nf_ip_phrase))
+    print('# of final ip phrases:')
+    print(len(final_ip_phrase))
+
+    print('# of phrases:')
+    print(len(nf_ip_phrase) + len(final_ip_phrase))
+    print('# of tons')
+    print(len(tokens))
     
+    print('nfinal_ip_tokens')
+    print(nfinal_ip_tokens)
+    print('final_ip_tokens')
+    print(final_ip_tokens)
 
-    
-    num_tokens = len(tokens)
-    num_phrases = len(phrases)
-    num_ips = len(ips)
-    num_vocab = len(vocab)
+    token_counts.update(tokens)    
 
-    print('# of tokens : %d' % num_tokens)
-    print('# of phrases: %d' % num_phrases)
-    print('# of ips: %d' % num_phrases)
-    print('# of types  : %d' % num_vocab)
-
-    # for i, t in enumerate(vocab):
-    #     print('%d %s '%(i,t))
-        
-    token_counts = Counter()
-    token_counts.update(tokens)
-
-    lm = defaultdict(Counter)
-    ending_lm = defaultdict(Counter)
-    ip_lm = defaultdict(Counter)
-
-
-
-    word_counts = Counter()
-
-    ending_counter = defaultdict(Counter)
-    ip_counter = defaultdict(Counter)
-    
+    # ALL
     for k,v in token_counts.items():
-        ending_counter[''][k] = v
+        uni_counter[k] = v
 
-    # Unigram
     for k,v in token_counts.items():
-        lm[''][k] = v / num_tokens
+        uni_lm[k] = v / len(tokens)
 
-    #Bigram(phrase)
-    for ph in phrases:
-        phr_list = ph.split()
-        for j in range(len(phr_list)-1):
-            ending_counter[phr_list[j]][phr_list[j+1]] += 1
-
-    for curw, freq_dict in ending_counter.items():
-        for nextw, freq in freq_dict.items():
-            if token_counts[curw] != 0:
-                ending_lm[curw][nextw] = freq_dict[nextw] / token_counts[curw]
-    print('---------------------------')
-    print('Ending Phrase')
-    print('---------------------------')
-    print('%6s | %6s | %s | %s' % ('Prev','Cur','freq', 'Prob'))
-    print('---------------------------')
-    for prev,d in ending_lm.items():
-        for cur,v in sorted(d.items(),key=lambda t: -t[1]):
-            print('%6s | %6s | %4d |%2.2f' % (prev, cur,ending_counter[prev][cur], ending_lm[prev][cur]*100))
-        print('---------------------------')                
-
-
-
-    #Bigram(ips)
-    for ph in ips:
+    # NF_IP_PHRASE(Uni, Bi)
+    for ph in nf_ip_phrase:
         phr_list = ph.split()
         for j in range(len(phr_list)-1):
             ip_counter[phr_list[j]][phr_list[j+1]] += 1
 
     for curw, freq_dict in ip_counter.items():
         for nextw, freq in freq_dict.items():
-            if token_counts[curw] != 0:
-                ip_lm[curw][nextw] = freq_dict[nextw] / token_counts[curw]
-    print('---------------------------')
-    print('IPs')
-    print('---------------------------')
-    print('%6s | %6s | %s | %s' % ('Prev','Cur','freq', 'Prob'))
-    print('---------------------------')
-    for prev,d in ip_lm.items():
-        for cur,v in sorted(d.items(),key=lambda t: -t[1]):
-            print('%6s | %6s | %4d |%2.2f' % (prev, cur,ip_counter[prev][cur], ip_lm[prev][cur]*100))
-        print('---------------------------')                
+            if nfinal_ip_tokens[curw] != 0:
+                ip_lm[curw][nextw] = freq_dict[nextw] / nfinal_ip_tokens[curw]
+
+    # ENDING(Uni, Bi)
+    for ph in final_ip_phrase:
+        phr_list = ph.split()
+        for j in range(len(phr_list)-1):
+            ending_counter[phr_list[j]][phr_list[j+1]] += 1
+
+    for curw, freq_dict in ending_counter.items():
+        for nextw, freq in freq_dict.items():
+            if final_ip_tokens[curw] != 0:
+                ending_lm[curw][nextw] = freq_dict[nextw] / final_ip_tokens[curw]
+
+    printDist('IP Distribution', ip_counter, ip_lm)
+    printDist('Ending Distribution', ending_counter, ending_lm)
 
 
+def printPitchAccentMatrix():
+    pass
+
+    
+
+def main()->None:
+    analyze()
+    printPitchAccentMatrix()
     
 main()
