87c4141e94e94a2581cd833290c6f1c46e25de18
Hee Hwang <heehcs@gmail.com>
2019-11-21 13:16:37 -0500

    Complete replicating Dainora's distribution

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from sys import stdin, stdout
 from collections import Counter
 from collections import defaultdict
 
+
+from prettytable import PrettyTable
+
+
 path = os.getcwd() + "/dainora/"
 stoptokens = ['HiF0', '*', '<', '>', '%r', '24.67']
 
-
+tokens = []
 nfinal_ip_tokens = Counter()
 final_ip_tokens = Counter()
 
@@ -28,20 +32,10 @@ ip_lm = defaultdict(Counter)
 ending_lm = defaultdict(Counter)
 
 
-def printDist(distname, counter, lm):
-    print('---------------------------')
-    print(distname)
-    print('---------------------------')
-    print('%6s | %6s | %s | %s' % ('Prev','Cur','freq', 'Prob'))
-    print('---------------------------')
-    for prev,d in lm.items():
-        for cur,v in sorted(d.items(),key=lambda t: -t[1]):
-            print('%6s | %6s | %4d |%2.2f' % (prev, cur, counter[prev][cur], lm[prev][cur]*100))
-        print('---------------------------')                
 
 
 def analyze() -> None:
-    tokens = []
+
 
     for filename in os.listdir(path):
         inf = open(path + filename, "r")
@@ -76,24 +70,6 @@ def analyze() -> None:
 
 
 
-    print('# of ip phrases:')
-    print(len(ip_phrase))
-
-    print('# of non-final ip phrases:')
-    print(len(nf_ip_phrase))
-    print('# of final ip phrases:')
-    print(len(final_ip_phrase))
-
-    print('# of phrases:')
-    print(len(nf_ip_phrase) + len(final_ip_phrase))
-    print('# of tons')
-    print(len(tokens))
-    
-    print('nfinal_ip_tokens')
-    print(nfinal_ip_tokens)
-    print('final_ip_tokens')
-    print(final_ip_tokens)
-
     token_counts.update(tokens)    
 
     # ALL
@@ -125,8 +101,66 @@ def analyze() -> None:
             if final_ip_tokens[curw] != 0:
                 ending_lm[curw][nextw] = freq_dict[nextw] / final_ip_tokens[curw]
 
-    printDist('IP Distribution', ip_counter, ip_lm)
-    printDist('Ending Distribution', ending_counter, ending_lm)
+
+def printNumbers():
+    print('# of ip phrases:')
+    print(len(ip_phrase))
+
+    print('# of non-final ip phrases:')
+    print(len(nf_ip_phrase))
+    print('# of final ip phrases:')
+    print(len(final_ip_phrase))
+
+    print('# of phrases:')
+    print(len(nf_ip_phrase) + len(final_ip_phrase))
+    print('# of tons')
+    print(len(tokens))
+    
+    # print('nfinal_ip_tokens')
+    # print(nfinal_ip_tokens)
+    # print('final_ip_tokens')
+    # print(final_ip_tokens)
+
+                
+def printDist(distname, counter, lm):
+
+
+    
+    print('---------------------------')
+    print(distname)
+    print('---------------------------')
+    print('%6s | %6s | %s | %s' % ('Prev','Cur','freq', 'Prob'))
+    print('---------------------------')
+    for prev,d in lm.items():
+        for cur,v in sorted(d.items(),key=lambda t: -t[1]):
+            print('%6s | %6s | %4d |%2.2f' % (prev, cur, counter[prev][cur], lm[prev][cur]*100))
+        print('---------------------------')                
+
+
+def printToneMatrix(distname, counter, lm):
+    print(distname + ' IP phrase transition')
+    t = PrettyTable()
+    domain = set()
+    image = set()
+    
+    for prev, d in counter.items():
+        domain.add(prev)
+        for cur, freq in d.items():
+            image.add(cur)
+
+    domain = list(domain)
+    image = list(image)
+        
+
+    t.field_names = ['\\'] + image
+    for prev in domain:
+        cur_row = []
+        cur_row.append(prev)
+        for cur in image:
+            cur_row.append(counter[prev][cur])
+        t.add_row(cur_row)
+            
+    print(t)
 
 
 def printPitchAccentMatrix():
@@ -136,6 +170,11 @@ def printPitchAccentMatrix():
 
 def main()->None:
     analyze()
-    printPitchAccentMatrix()
+    printNumbers()
+    # printDist('Non-final IP Distribution', ip_counter, ip_lm)
+    # printDist('Final IP Distribution', ending_counter, ending_lm)
+    printToneMatrix("Non-final", ip_counter, ip_lm)
+    printToneMatrix("Final", ending_counter, ending_lm)
+    #printPitchAccentMatrix()
     
 main()
